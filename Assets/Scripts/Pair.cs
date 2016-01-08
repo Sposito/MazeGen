dc915b1979f6872e48b6b31395377658e922d85c
Sposito <sposito.thiago@gmail.com>
2016-01-08 20:20:56 -0200

    Pair Class  Creation

---
+++ Assets/Scripts/Pair.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+public class Pair  {
+	public int x, y;
+
+	public Pair(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public static Pair operator + (Pair p1, Pair p2){
+		return new Pair (p1.x + p2.x, p1.y + p2.y);
+	}
+
+
+}
