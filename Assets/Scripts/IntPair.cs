2c2536874c14dbc720c298229ec898330b27deda
Sposito <sposito.thiago@gmail.com>
2016-01-08 22:38:45 -0200

    IntPair Operator updates
    Refactor the name f the class and added static members and operators

---
+++ Assets/Scripts/IntPair.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System;
+
+public class Pair  {
+	public int x, y;
+
+	public readonly static Pair up    = new Pair( 0, 1);
+	public readonly static Pair right = new Pair( 1, 0);
+	public readonly static Pair down  = new Pair( 0,-1);
+	public readonly static Pair left  = new Pair(-1, 0);
+	public readonly static Pair zero  = new Pair( 0, 0);
+	public readonly static Pair one   = new Pair( 1, 1);
+
+
+	public Pair(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public static Pair operator + (Pair p1, Pair p2){
+		return new Pair (p1.x + p2.x, p1.y + p2.y);
+	}
+
+	public static Pair operator * (Pair p1, int m){
+		return new Pair (p1.x * m, p1.y * m);
+	}
+
+	public static Pair operator * (int m ,Pair p1){
+		return new Pair (p1.x * m, p1.y * m);
+	}
+
+	public override string ToString(){
+		return(String.Format("({0}, {1})", x, y));
+	}
+
+
+
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print (2 * new Pair (1, 2));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
